93d7714ae64f47a2494245f723369e13413bd21a
Yousef Sedik <joemohmmed00@gmail.com>
2024-08-29 17:42:10 +0300

    added problem app routes.

--- problem/router.py
+++ problem/router.py
@@ -2,3 +2,18 @@ from fastapi.routing import APIRouter
 
 
 router = APIRouter()
+
+
+@router.get("/problem/<problem_id>")
+async def get_problem(problem_id: int):
+    return {"problem_id": problem_id}
+
+
+@router.post("/problem/manual")
+async def create_problem():
+    return {"message": "Problem created successfully"}
+
+
+@router.post("/problem/CSES")
+async def create_problem_cses():
+    return {"message": "Problem created successfully"}
